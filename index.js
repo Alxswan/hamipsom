10eb4bdd3d898b1071799440bda3a97e9ff41c24
Alex Swan <alex.swan@ansarada.com>
2016-02-05 16:58:31 +1100

    Refactoring

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
 var alexander = ["How does a bastard, orphan, son of a whore and a Scotsman, dropped in the middle of a Forgotten spot in the Caribbean by providence Impoverished, in squalor Grow up to be a hero and a scholar?   The ten-dollar founding father without a father Got a lot farther by working a lot harder By being a lot smarter By being a self-starter By fourteen, they placed him in charge of a Trading charter.",   "And every day while slaves were being slaughtered and carted Away across the waves, he struggled and kept his guard up Inside, he was longing for something to be a part of The brother was ready to beg, steal, borrow, or barter.   Then a hurricane came, and devastation reigned Our man saw his future drip, dripping down the drain Put a pencil to his temple, connected it to his brain And he wrote his first refrain, a testament to his pain",   "Well, the word got around, they said, “This kid is insane, man” Took up a collection just to send him to the mainland “Get your education, don’t forget from whence you came, and The world is gonna know your name. What’s your name, man?”   Alexander Hamilton My name is Alexander Hamilton And there’s a million things I haven’t done But just you wait, just you wait...",   "When he was ten his father split, full of it, debt-ridden Two years later, see Alex and his mother bed-ridden Half-dead sittin in their own sick, the scent thick   And Alex got better but his mother went quick   Moved in with a cousin, the cousin committed suicide Left him with nothin’ but ruined pride, something new inside A voice saying “Alex, you gotta fend for yourself.”   He started retreatin’ and readin’ every treatise on the shelf",   "There would have been nothin’ left to do For someone less astute He woulda been dead or destitute Without a cent of restitution Started workin’, clerkin’ for his late mother’s landlord Tradin’ sugar cane and rum and all the things he can’t afford Scammin’ for every book he can get his hands on Plannin’ for the future see him now as he stands on The bow of a ship headed for a new land In New York you can be a new man.",   "Alexander Hamilton We are waiting in the wings for you You could never back down You never learned to take your time! Oh, Alexander Hamilton When America sings for you Will they know what you overcame? Will they know you rewrote the game? The world will never be the same, oh   The ship is in the harbor now See if you can spot him Another immigrant Comin’ up from the bottom His enemies destroyed his rep America forgot him.",   "Alexander Hamilton we are waiting in the wings for you You never learned to take your time! Oh, Alexander Hamilton Alexander Hamilton… America sings for you Will they know what you overcame? Will they know you rewrote the game? The world will never be the same, oh   Just you wait   Just you wait     We fought with him   Me? I died for him   Me? I trusted him   Me? I loved him   And me? I’m the damn fool that shot him   There’s a million things I haven’t done But just you wait!   What’s your name, man?   Alexander Hamilton!"] 
 var myShot = ["I am not throwing away my shot! I am not throwing away my shot! Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot! I’m ‘a get a scholarship to King’s College I prob’ly shouldn’t brag, but dag, I amaze and astonish The problem is I got a lot of brains but no polish I gotta holler just to be heard With every word, I drop knowledge! I’m a diamond in the rough, a shiny piece of coal Tryin’ to reach my goal. My power of speech: unimpeachable Only nineteen but my mind is older These New York City streets get colder, I shoulder Ev’ry burden, ev’ry disadvantage I have learned to manage, I don’t have a gun to brandish I walk these streets famished The plan is to fan this spark into a flame But damn, it’s getting dark, so let me spell out the name", "I am the—   A-L-E-X-A-N-D E-R—we are—meant to be…   A colony that runs independently Meanwhile, Britain keeps shittin’ on us endlessly Essentially, they tax us relentlessly Then King George turns around, runs a spending spree He ain’t ever gonna set his descendants free So there will be a revolution in this century Enter me!   (He says in parentheses)   Don’t be shocked when your hist’ry book mentions me I will lay down my life if it sets us free Eventually, you’ll see my ascendancy.",   "And I am not throwing away My shot I am not throwing away My shot Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot	   My shot!  My shot!   And I’m not throwing away my shot.    I am not throwing away my shot I am not throwing away my shot Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot It’s time to take a shot!   I dream of life without a monarchy The unrest in France will lead to ‘onarchy? ‘Onarchy? How you say, how you say, ‘anarchy?’ When I fight, I make the other side panicky With my—   Shot!",   "Yo, I’m a tailor’s apprentice And I got y’all knuckleheads in loco parentis I’m joining the rebellion cuz I know it’s my chance To socially advance, instead of sewin’ some pants! I’m gonna take a—   Shot!   But we’ll never be truly free Until those in bondage have the same rights as you and me You and I. Do or die. Wait till I sally in On a stallion with the first black battalion Have another—   Shot!   Geniuses, lower your voices You keep out of trouble and you double your choices I’m with you, but the situation is fraught You’ve got to be carefully taught: If you talk, you’re gonna get shot!",   "Burr, check what we got Mister Lafayette, hard rock like Lancelot I think your pants look hot Laurens, I like you a lot Let’s hatch a plot blacker than the kettle callin’ the pot... What are the odds the gods would put us all in one spot Poppin’ a squat on conventional wisdom, like it or not A bunch of revolutionary manumission abolitionists? Give me a position, show me where the ammunition is!  Oh, am I talkin’ too loud? Sometimes I get over excited, shoot off at the mouth I never had a group of friends before I promise that I’ll make y’all proud   Let’s get this guy in front of a crowd   I am not throwing away my shot I am not throwing away my shot Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot.",  "I am not throwing away my shot I am not throwing away my shot Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot   Ev’rybody sing: Whoa, whoa, whoa Hey! Whoa! Wooh!! Whoa! Ay, let ‘em hear ya!  Let’s go!   I said shout it to the rooftops! Said, to the rooftops! Come on!  Come on, let’s go!	 Whoa! Whoa! Whoa!  Whoa!  Whoa!  Yea!   Whoa! Whoa! Whoa! Whoa! Whoa!  Yea!",    "Rise up! When you’re living on your knees, you rise up Tell your brother that he’s gotta rise up Tell your sister that she's gotta rise up   When are these colonies gonna rise up? When are these colonies gonna rise up? When are these colonies gonna rise up? When are these colonies gonna rise up?  Rise up!	   Whoa! Whoa! Whoa! Whoa! Whoa!  Rise up!    I imagine death so much it feels more like a memory When’s it gonna get me? In my sleep? Seven feet ahead of me? If I see it comin’, do I run or do I let it be? Is it like a beat without a melody? See, I never thought I’d live past twenty Where I come from some get half as many Ask anybody why we livin’ fast and we laugh, reach for a flask We have to make this moment last, that’s plenty.",  "Scratch that This is not a moment, it’s the movement Where all the hungriest brothers with Something to prove went? Foes oppose us, we take an honest stand We roll like Moses, claimin’ our promised land And? If we win our independence? Is that a guarantee of freedom for our descendants? Or will the blood we shed begin an endless Cycle of vengeance and death with no defendants? I know the action in the street is excitin’ But Jesus, between all the bleedin’ ‘n fightin’ I’ve been readin’ ‘n writin’ We need to handle our financial situation Are we a nation of states? What’s the state of our nation? I’m past patiently waitin’. I’m passionately Smashin’ every expectation Every action’s an act of creation! I’m laughin’ in the face of casualties and sorrow For the first time, I’m thinkin’ past tomorrow",   "And I am not throwing away my shot I am not throwing away my shot Hey yo, I’m just like my country I’m young, scrappy and hungry And I’m not throwing away my shot   We’re gonna rise up! Time to take a shot! We’re gonna    Time to take a shot!   Time to take a shot!  Time to take a shot!  Take a shot! Shot! Shot! A-yo it’s  Time to take a shot! Time to take a shot! And I am—  Not throwin’ away my— Not throwin’ away my shot!"]
- var wait = ["Theodosia writes me a letter everyday I'm keeping the bed warm while her husband is away He's on the British side in Georgia He's trying to keep the colonies in line But he can keep all of Georgia Theodosia, she's mine  Love doesn't discriminate Between the sinners And the saints It takes and it takes and it takes And we keep loving anyway We laugh and we cry And we break And we make our mistakes And if there's a reason I'm by her side When so many have tried Then I'm willing to wait for it I'm willing to wait for it.",    "My grandfather was a fire and brimstone preacher     But there are things that the Homilies and hymns won't teach ya     My mother was a genius   My father commanded respect   When they died they left no instructions Just a legacy to protect  	    Death doesn’t discriminate Between the sinners And the saints It takes and it takes and it takes And we keep living anyway We rise and we fall And we break And we make our mistakes And if there’s a reason I’m still alive When everyone who loves me has died I’m willing to wait for it I’m willing to wait for it",  "Wait for it    I am the one thing in life I can control    Wait for it    I am inimitable I am an original    Wait for it     I’m not falling behind or running late    Wait for it     I’m not standing still I am lying in wait   Hamilton faces an endless uphill climb    He has something to prove He has nothing to lose     Hamilton’s pace is relentless He wastes no time    What is it like in his shoes?",  "Hamilton doesn’t hesitate He exhibits no restraint He takes and he takes and he takes And he keeps winning anyway He changes the game He plays and he raises the stakes And if there’s a reason He seems to thrive when so few survive, then Goddamnit—    I'm willing to wait for it     I'm willing to wait for it...",   "Life doesn't discriminate Between the sinners and the saints It takes and it takes and it takes   We rise We fall   And if there's a reason I'm still alive When so many have died Then I'm willin' to—  	  I'm willing to wait for it Wait for it Wait for... I'm willing to—   Life doesn't discriminate Between the sinners and the saints It takes and it takes and it takes And we keep living anyway We rise and we fall and we break And we make our mistakes And if there's a reason I'm still alive When so many have died Then I'm willin' to—     Wait for it...     Wait for it..."]
-var hamilton = alexander.concat(myShot, wait)
+var wait = ["Theodosia writes me a letter everyday I'm keeping the bed warm while her husband is away He's on the British side in Georgia He's trying to keep the colonies in line But he can keep all of Georgia Theodosia, she's mine  Love doesn't discriminate Between the sinners And the saints It takes and it takes and it takes And we keep loving anyway We laugh and we cry And we break And we make our mistakes And if there's a reason I'm by her side When so many have tried Then I'm willing to wait for it I'm willing to wait for it.",    "My grandfather was a fire and brimstone preacher     But there are things that the Homilies and hymns won't teach ya     My mother was a genius   My father commanded respect   When they died they left no instructions Just a legacy to protect  	    Death doesn’t discriminate Between the sinners And the saints It takes and it takes and it takes And we keep living anyway We rise and we fall And we break And we make our mistakes And if there’s a reason I’m still alive When everyone who loves me has died I’m willing to wait for it I’m willing to wait for it",  "Wait for it    I am the one thing in life I can control    Wait for it    I am inimitable I am an original    Wait for it     I’m not falling behind or running late    Wait for it     I’m not standing still I am lying in wait   Hamilton faces an endless uphill climb    He has something to prove He has nothing to lose     Hamilton’s pace is relentless He wastes no time    What is it like in his shoes?",  "Hamilton doesn’t hesitate He exhibits no restraint He takes and he takes and he takes And he keeps winning anyway He changes the game He plays and he raises the stakes And if there’s a reason He seems to thrive when so few survive, then Goddamnit—    I'm willing to wait for it     I'm willing to wait for it...",   "Life doesn't discriminate Between the sinners and the saints It takes and it takes and it takes   We rise We fall   And if there's a reason I'm still alive When so many have died Then I'm willin' to—  	  I'm willing to wait for it Wait for it Wait for... I'm willing to—   Life doesn't discriminate Between the sinners and the saints It takes and it takes and it takes And we keep living anyway We rise and we fall and we break And we make our mistakes And if there's a reason I'm still alive When so many have died Then I'm willin' to—     Wait for it...     Wait for it..."]
+
+var hamilton = [
+	alexander,
+	myShot,
+	wait
+];
+
+var lyrics = [];
+
+hamilton.forEach(function (song) {
+	song.forEach(function (paragraph) {
+		lyrics.push(paragraph);
+	});
+});
 
 $(document).ready(function(){
 
+	var $lorem = $('#lorem'),
+		$select = $('select'),
+		$input = $('input');
+		lyricsLength = lyrics.length;
+
 	$('button').on('click', function() {
 
-	$('.lorem').empty();
 
-	var numParagraphs = $('select').val()
-	var random = Math.floor((Math.random() * (hamilton.length - numParagraphs))	);
-	for (var i = 0; i < numParagraphs; i++) {
+		var numParagraphs = parseInt($select.val(), 10);
 
-	if ($('input:checked').val() == "random")
-	{
-	random = Math.floor((Math.random() * (hamilton.length))	);
+		// Start data set
+		var newParagraphs = (new Array(numParagraphs)).fill(true);
 
-		$('#p' + i).append(hamilton[random]);
-		//random += 1;
-	}
-	else 
-	
-		$('#p' + i).append(hamilton[random]);
-		random += 1;
-	
 
-	}
+		// Check for random or non random option
+		if ($input.filter(':checked').val() === 'random') {
+			// Get random data
+			newParagraphs = newParagraphs.map(function () {
+				var fetchIndex =  Math.floor((Math.random() * (lyricsLength)))
+				return lyrics[ fetchIndex ];
+			});
 
-})	
+		} else {
+			var fetchOffset =  Math.abs(Math.floor((Math.random() * (lyricsLength - numParagraphs))));
+			// Get sequential data
+
+			newParagraphs = newParagraphs.map(function (item, index) {
+				var fetchIndex  = (index + fetchOffset) % lyricsLength;
+				return lyrics[ fetchIndex ];
+			});
+
+		}
+		
+		// Generate paragraph dom
+		newParagraphs = newParagraphs
+		.map(function (pText) {
+			return '<p>' + pText + '</p>';
+		})
+		.reduce(function(previousValue, currentValue) {
+		  return previousValue + currentValue;
+		});;
+
+
+		// Update dom
+		$lorem.html( newParagraphs );
+
+
+	})	
 
 });
